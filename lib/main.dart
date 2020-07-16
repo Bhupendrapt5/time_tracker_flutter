3e12d385ac14b80e02a4b437564d688d83f19e50
bhupendrapt <bhupendrapt95@gmail.com>
2020-07-16 10:11:33 +0530

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker/app/sign_in/sign_in_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignInScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
